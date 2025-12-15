d07322af7253eebaaf70bc477da329a9d1861c83
admin <admin@gitea.local>
2025-12-15 02:40:48 +0100

    Initial Commit

---
+++ ThinkFirst/ThinkFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkFirstApp.swift
+//  ThinkFirst
+//
+//  Created by psporidi on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThinkFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
