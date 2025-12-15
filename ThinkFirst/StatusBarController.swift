d7d501481df4823c8c6898104f7cb96c74f57183
admin <admin@gitea.local>
2025-12-15 09:19:01 +0100

    publish

--- ThinkFirst/StatusBarController.swift
+++ ThinkFirst/StatusBarController.swift
@@ -5,18 +5,34 @@
 import SwiftUI
 import AppKit
 
-class StatusBarController {
+final class StatusBarController: NSObject, NSMenuDelegate {
     private var statusItem: NSStatusItem!
     private var stickyNoteWindowController: StickyNoteWindowController? = nil
+    private let statusMenu = NSMenu()
+    private let toggleVisibilityItem = NSMenuItem()
 
-    init() {
+    override init() {
+        super.init()
         stickyNoteWindowController = StickyNoteWindowController()
+
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let button = statusItem.button {
             button.image = NSImage(systemSymbolName: "globe", accessibilityDescription: "ThinkFirst")
-            button.action = #selector(toggleStickyNote)
-            button.target = self
         }
+
+        statusMenu.delegate = self
+        toggleVisibilityItem.target = self
+        toggleVisibilityItem.action = #selector(toggleStickyNoteFromMenu)
+        statusMenu.addItem(toggleVisibilityItem)
+        statusMenu.addItem(.separator())
+
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
+        quitItem.keyEquivalentModifierMask = [.command]
+        quitItem.target = self
+        statusMenu.addItem(quitItem)
+
+        updateToggleVisibilityTitle()
+        statusItem.menu = statusMenu
     }
 
     func showStickyNote() {
@@ -27,12 +43,29 @@ class StatusBarController {
         stickyNoteWindowController?.hideStickyNote()
     }
 
-    @objc private func toggleStickyNote() {
+    func menuWillOpen(_ menu: NSMenu) {
+        updateToggleVisibilityTitle()
+    }
+
+    private func updateToggleVisibilityTitle() {
+        guard let controller = stickyNoteWindowController else {
+            toggleVisibilityItem.title = "Show"
+            return
+        }
+        toggleVisibilityItem.title = controller.isStickyNoteVisible ? "Hide" : "Show"
+    }
+
+    @objc private func toggleStickyNoteFromMenu() {
         guard let controller = stickyNoteWindowController else { return }
         if controller.isStickyNoteVisible {
             controller.hideStickyNote()
         } else {
             controller.showStickyNote()
         }
+        updateToggleVisibilityTitle()
+    }
+
+    @objc private func quitApp() {
+        NSApp.terminate(nil)
     }
 }
